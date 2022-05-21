212156b1939bae76fe0eb8acaa41b79453c58f62
Sreynaj Keb <kebsreynaj2@gmail.com>
2022-05-21 17:03:11 +0700

    Sourcecode

---
+++ Calculator.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main(){
+    char operator; 
+    int number1, number2, total_number ; 
+    printf("A mini calculator!\n");
+    printf("\nYou can calculate that by \n\n - Choose one of the operator + - x / \n\n - And then your first and sencond number!\n");
+    printf("\n Choose one operater: ");
+    operator = getchar();
+
+
+ switch(operator){
+     case '+' :
+     printf(" \n Enter your first number: "); 
+     scanf("%d" , &number1) ;
+     printf(" \n Enter your second number: "); 
+     scanf("%d" , &number2) ;
+     total_number = number1 + number2 ;
+     printf("TOTAL: " "%d + %d = %d \n" , number1,number2, total_number) ; 
+     break;
+
+     case '-' : 
+     printf("Enter your first number:"); 
+     scanf("%d" , &number1) ;
+     printf("Enter your second number:"); 
+     scanf("%d" , &number2) ;
+     total_number = number1 - number2 ;
+     printf(" \n TOTAL: " "%d - %d = %d \n" , number1,number2, total_number) ; 
+     break;
+
+
+     case 'x': 
+     printf("Enter your first number:"); 
+     scanf("%d" , &number1) ;
+     printf("Enter your second number:"); 
+     scanf("%d" , &number2) ;
+     total_number = number1 * number2 ;
+     printf("TOTAL: " "%d x %d = %d \n" , number1,number2, total_number) ; 
+     break;
+
+     case '/': 
+     printf("Enter your first number:"); 
+     scanf("%d" , &number1) ;
+     printf("Enter your second number:"); 
+     scanf("%d" , &number2) ;
+     total_number = number1 / number2 ;
+     printf("TOTAL: " "%d / %d = %d \n" , number1,number2, total_number) ; 
+     break;
+ }
+
+
+}
